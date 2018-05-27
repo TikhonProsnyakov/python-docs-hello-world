f6c40a4d80d4af9393979ef03626d48303cd9d7b
Tikhon Prosnyakov <tihon007@mail.ru>
2018-05-27 23:42:16 +0200

    mongo

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from flask import Flask
+import pymongo
+
+
 app = Flask(__name__)
+
+mongo_uri = 'mongodb://developer:cosy@ds016098.mlab.com:16098/bot'
+conn = pymongo.MongoClient(mongo_uri)
+db = conn['bot']
+user_collection = db["users"]
 
 @app.route('/')
 def hello_world():
